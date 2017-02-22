4698dd7064863340dc479243fc7175767003d28c
Xuanyu Zhou <zhouxuanyu.personal@gmail.com>
2017-02-22 17:05:21 -0600

    add secret

---
+++ database/secret.py
@@ -0,0 +1,8 @@
+def get_server_key():
+	return 'movierecommender.database.windows.net'
+def get_database_key():
+	return 'MovieRec'
+def get_username_key():
+	return 'learner'
+def get_password_key():
+	return 'MR2017sql'
