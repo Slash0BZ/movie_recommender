35f4078662cc726af2cd46aeeb01e05bee72d523
Xuanyu Zhou <zhouxuanyu.personal@gmail.com>
2017-02-23 12:10:13 -0600

    fix parser

--- database/database_util.py
+++ database/database_util.py
@@ -17,6 +17,8 @@ class database:
 	cursor = object()
 	cnxn = object()
 
+	# The init funciton for class database
+	# Get called everytime a database object is created
 	def __init__(self, _server = server, _database = database, _username = username,_password = password):
 		self.server = _server
 		self.database = _database
@@ -24,18 +26,27 @@ class database:
 		self.password = _password
 		self.connect()
 	
+	# Get callde everytime a database object is collected
 	def __del__(self):
 		self.cursor.close()
 		del self.cursor
 		self.cnxn.close()
 	
+	# Connect to the database.
+	# Set two members of the database class
+	# cnxn: the connection to the database
+	# cursor: the cursor to the connection
+	# More info: search Microsoft pyodbc
 	def connect(self):
 		self.cnxn = pyodbc.connect('DRIVER='+self.driver+';PORT=1433;SERVER='+self.server+';PORT=1443;DATABASE='+self.database+';UID='+self.username+';PWD='+ self.password)
 		self.cursor = self.cnxn.cursor()
 
+	# Disconnect to the database.
+	# This get called in the deletion function for the class
 	def disconnect(self):
 		self.cursor.close()
 
+	# Write a msg with time to the end of the log_file
 	def write_log(self, msg):
 		time_string = str(datetime.datetime.now())
 		prepared_string = "[%s]: %s" % (time_string, msg)
@@ -43,6 +54,7 @@ class database:
 		f.write(prepared_string)
 		f.close()
 
+	# Return True if the (name, genre, year) tuple exists in movie_info 
 	def exists_in_movie_info(self, name, genre, year):
 		self.cursor.execute("SELECT * FROM movie_info WHERE name=? AND genre=? AND year=?", name, genre, year)
 		row = self.cursor.fetchone()
@@ -51,11 +63,23 @@ class database:
 		else:
 			return False
 
-	def add_movie(self, name, genre, year):
+	# Add a movie to movie_info with name, genre, year
+	# Write log after the operation finishes
+	def add_movie(self, m_id, name, genre, year):
 		if (self.exists_in_movie_info(name, genre, year)):
 			self.write_log("INSERT FAILURE DUE TO EXISTION %s %s %s" % (name, genre, year))
 			return
-		self.cursor.execute("INSERT INTO movie_info (name,genre,year) VALUES (?, ?, ?)", name, genre, year)
+		self.cursor.execute("INSERT INTO movie_info (id,name,genre,year) VALUES (?,?,?,?)",m_id, name, genre, year)
 		self.cnxn.commit()
 		self.write_log("INSERT movie_info %s %s %s" % (name, genre, year))
 
+	# Create a new column in the specified table
+	# Do nothing if the column_name is invalid or already exists
+	# Do nothing if column_type is invalid
+	# Write log message
+	def create_column(self, table_name, column_name, column_type):
+		# TO DO
+		self.cursor.execute("ALTER TABLE ? ADD ? ?", table_name, column_name, column_type)
+
+	# Add info to the specifies rows in the specifies column with value	
+	#def add_movie_additional(self, m_id, column_name, value):
--- database/parser.py
+++ database/parser.py
@@ -12,14 +12,22 @@ def import_movie():
 		if (count == 1):
 			continue
 		line = line.replace("\r\n", "")
-		featureList = line.split(',')
-
+		featureList = []
+		if (line.find("\"") != -1):
+			featureList = line.split('"')
+			featureList[0] = featureList[0].replace(",", "")
+			featureList[2] = featureList[2].replace(",", "")
+		else:
+			featureList = line.split(',')
+		
+		m_id = featureList[0]
 		name = featureList[1]
 		year = featureList[1]
 		genre = featureList[2]
 		name = name[0:name.find("(") - 1]
-		year = year[year.find("(")+1:year.find(")")]
-		db.add_movie(name, genre, year)
+		pattern = re.compile(r'\((\d\d\d\d)\)', flags=re.DOTALL)
+		year = pattern.findall(year)[0]
+		db.add_movie(m_id, name, genre, year)
 	f.close()
 	
 import_movie()
