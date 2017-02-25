901f3a71e0df2afd9613c021198d516566e48eb6
Xuanyu Zhou <zhouxuanyu.personal@gmail.com>
2017-02-25 01:06:47 -0600

    fix empty year in import movie

--- database/parser.py
+++ database/parser.py
@@ -2,15 +2,13 @@ import database_util
 import re
 
 db = database_util.database()
-movie_info_file = './data/movies.csv'
+movie_info_file = '../data/movies.csv'
 
 def import_movie():
 	count = 0
 	f = open(movie_info_file, 'r')
 	for line in f:
 		count = count + 1
-		if (count == 1):
-			continue
 		line = line.replace("\r\n", "")
 		featureList = []
 		if (line.find("\"") != -1):
@@ -26,7 +24,11 @@ def import_movie():
 		genre = featureList[2]
 		name = name[0:name.find("(") - 1]
 		pattern = re.compile(r'\((\d\d\d\d)\)', flags=re.DOTALL)
-		year = pattern.findall(year)[0]
+		year = pattern.findall(year)
+		if (len(year) > 0):
+			year = year[0]
+		else:
+			year = ""
 		db.add_movie(m_id, name, genre, year)
 	f.close()
 	
