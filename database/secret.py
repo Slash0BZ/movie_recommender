474de04e24f6ea4c79a57b4860b7d57a11de4db4
Xuanyu Zhou <zhouxuanyu.personal@gmail.com>
2017-03-06 10:54:50 -0600

    remove files containing password

--- database/secret.py
+++
@@ -1,8 +0,0 @@
-def get_server_key():
-	return 'movierecommender.database.windows.net'
-def get_database_key():
-	return 'MovieRec'
-def get_username_key():
-	return 'learner'
-def get_password_key():
-	return 'MR2017sql'
