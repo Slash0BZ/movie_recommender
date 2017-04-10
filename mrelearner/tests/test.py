2c491d39459aa10b08887aabe4ee0868631f45bd
Xuanyu Zhou <zhouxuanyu.personal@gmail.com>
2017-04-10 00:38:07 -0500

    add codecov

--- mrelearner/tests/test.py
+++ mrelearner/tests/test.py
@@ -1,1 +1,10 @@
 # The test script that is going to be run be unit testers
+import unittest
+
+class TestMethods(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(1, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
